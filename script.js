6cc364e7c06d042db7fdcb27dbe08890126f0179
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-02-15 20:42:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const parseCode = (str) => {
   // your code here
+	let name = ""
+	let indL;
+	for(let i = 0 ; i<str.length ; i++){
+		if(str[i]==='0'){
+			indL = i;
+			return ;
+		}
+		name+=str[i];
+	}
+	let lname = ""
+	let indR ;
+	for(let i = str.length-1 ; i>=0 ; i--){
+		if(str[i]==='0'){
+			indR = i;
+			return ;
+		}
+		lname+=str[i];
+	}
+	let indR ;
+	let idx="";
+	for(let i = indL ; i<indR ; i++){
+		if(str[i]!=='0'){
+			
+			idx+=str[i] ;
+		}
+		
+	}
+	let obj = {
+		firstName:name,
+		lastName:lname,
+		id:idx,
+}
+	return obj;
+	
 };
 
-// Do not change the code below
-const str = prompt("Enter str: ");
+// // Do not change the code below
+// const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
